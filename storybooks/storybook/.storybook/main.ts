c1d22523fb1e6c4cc541f2c6cecdeebed6b64a14
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-06-10 23:44:23 +0900

    storybook: setup config

--- storybooks/storybook/.storybook/main.ts
+++ storybooks/storybook/.storybook/main.ts
@@ -1,6 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    {
+      titlePrefix: 'ui',
+      directory: '../../../packages/ui/components/*',
+      files: '*.stories.@(js|jsx|ts|tsx)',
+    },
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
