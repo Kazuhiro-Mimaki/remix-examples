a50b64b68808053d11b742e538678dfcac1bef67
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-07-10 09:21:44 +0900

    apps/panda-css: get started

---
+++ apps/panda-css/app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+import { css } from "styled-system/css";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+      Hello 🐼!
+    </div>
+  );
+}
